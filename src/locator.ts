dda3644f9f60cb551d6d643af10044efdeaed322
Bruce D'Arcus <bdarcus@gmail.com>
2023-04-17 11:39:26 -0400

    feat: add start of locators

---
+++ src/locator.ts
@@ -0,0 +1,14 @@
+// CSL locators model
+// A locator is a simple key-value object, where the value is some potentially discontinuos list of numbers and/or strings.
+// Examples:
+//   - page 1
+//   - page 23, 25-36
+//   - chapter IV
+
+type Locator = object;
+
+// REVIEW this approach is more human-friendly, but maybe better to just have explicit type/value properties?
+// In any case, this is just a place holder ATM.
+interface Pages {
+    pages: (Locator | Locator[]);
+}

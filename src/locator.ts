7a03701a86f9dba0edef3b345f9d1a386ecfb596
Bruce D'Arcus <bdarcus@gmail.com>
2023-04-20 05:12:07 -0400

    style: formatting of src

--- src/locator.ts
+++ src/locator.ts
@@ -5,11 +5,11 @@
 //   - page 23, 25-36
 //   - chapter IV
 
-type Locator = (string | number | number[]);
+type Locator = string | number | number[];
 
 // REVIEW this approach is more human-friendly, but maybe better to just have explicit type/value properties?
 // In any case, this is just a place holder ATM.
 interface Locators {
-    pages?: Locator;
-    chapter?: Locator;
+  pages?: Locator;
+  chapter?: Locator;
 }

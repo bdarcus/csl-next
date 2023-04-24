86985332b8d09025bdcdf8bacc2c6a5a819ba6a3
Bruce D'Arcus <bdarcus@gmail.com>
2023-04-24 14:32:01 -0400

    docs(style): add docs (and type) for citation modes
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- src/citation.ts
+++ src/citation.ts
@@ -1,10 +1,29 @@
 
 import { CiteRef } from './citeref';
 
+type CitationModeType = "integral" | "nonIntegral";
+
 export type Citation = {
-  mode?: "integral" | "nonintegral";
+  /**
+   * Local citation rendering option; aka command or style.
+   * 
+   * - `integral` places the author inline in the text; also knows as "narrative" or "in text" citations.
+   * - `nonIntegral` places the author in the citation.
+   * 
+   * Both are more general than author-date styles, and can apply to any citation style.
+   * 
+   * @default nonIntegral // REVIEW this is current CSL behavior, but should it be? Remove the default altogether?
+   * 
+   */
+  mode?: CitationModeType;
+  /**
+   * The string that prefaces a list of citation references.
+   */
   // richer than CSL 1.0, but matches biblatex/org-cite
   prefix?: string;
+  /**
+   * The string that follows a list of citation references.   
+   */
   suffix?: string;
   references: CiteRef[];
 }

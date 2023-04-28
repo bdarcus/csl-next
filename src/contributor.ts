203e1d628c3b4665b773b7f9e584e1c804f70d55
Bruce D'Arcus <bdarcus@gmail.com>
2023-04-28 15:44:45 -0400

    chore: add CI, tweak package
    Close: #20, #53
    
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- src/contributor.ts
+++ src/contributor.ts
@@ -1,41 +1,39 @@
-
-
 // contributor modeling needs more thought in general
 
 export type Contributor = Person | Organization;
 
 export class Organization {
-  name: string;
-  location?: string;
+	name: string;
+	location?: string;
 
-  constructor(name: string, location?: string) {
-    this.name = name;
-    this.location = location;
-  }
+	constructor(name: string, location?: string) {
+		this.name = name;
+		this.location = location;
+	}
 
-  getSortName(): string {
-    return this.name;
-  }
+	getSortName(): string {
+		return this.name;
+	}
 }
 
-export class Person  {
-    familyName: string;
-    givenName: string;
+export class Person {
+	familyName: string;
+	givenName: string;
 
-    constructor(givenName: string, familyName: string) {
-      this.givenName = givenName;
-      this.familyName = familyName;
-    }
+	constructor(givenName: string, familyName: string) {
+		this.givenName = givenName;
+		this.familyName = familyName;
+	}
 
-    getFullName(): string {
-      return `${this.givenName} ${this.familyName}`;
-    }
+	getFullName(): string {
+		return `${this.givenName} ${this.familyName}`;
+	}
 
-    getGivenInitial(): string {
-      return `${this.givenName[0]}`;
-    }
+	getGivenInitial(): string {
+		return `${this.givenName[0]}`;
+	}
 
-    getSortName(): string {
-      return `${this.familyName}, ${this.givenName}`;
-    }
-  }
+	getSortName(): string {
+		return `${this.familyName}, ${this.givenName}`;
+	}
+}
--- src/locale.ts
+++ src/locale.ts
@@ -1,74 +1,74 @@
 // REVIEW not sure where best to do this
 type Lang =
-  | "af-ZA"
-  | "ar"
-  | "bg-BG"
-  | "ca-AD"
-  | "cs-CZ"
-  | "da-DK"
-  | "de-AT"
-  | "de-CH"
-  | "de-DE"
-  | "el-GR"
-  | "en-GB"
-  | "en-US"
-  | "es-ES"
-  | "et-EE"
-  | "eu"
-  | "fa-IR"
-  | "fi-FI"
-  | "fr-CA"
-  | "fr-FR"
-  | "he-IL"
-  | "hr-HR"
-  | "hu-HU"
-  | "is-IS"
-  | "it-IT"
-  | "ja-JP"
-  | "km-KH"
-  | "ko-KR"
-  | "lt-LT"
-  | "lv-LV"
-  | "mn-MN"
-  | "nb-NO"
-  | "nl-NL"
-  | "pl-PL"
-  | "pt-BR"
-  | "pt-PT"
-  | "ro-RO"
-  | "ru-RU"
-  | "sk-SK"
-  | "sl-SI"
-  | "sr-RS"
-  | "sv-SE"
-  | "th-TH"
-  | "tr-TR"
-  | "uk-UA"
-  | "vi-VN"
-  | "zh-CN"
-  | "zh-TW";
+	| "af-ZA"
+	| "ar"
+	| "bg-BG"
+	| "ca-AD"
+	| "cs-CZ"
+	| "da-DK"
+	| "de-AT"
+	| "de-CH"
+	| "de-DE"
+	| "el-GR"
+	| "en-GB"
+	| "en-US"
+	| "es-ES"
+	| "et-EE"
+	| "eu"
+	| "fa-IR"
+	| "fi-FI"
+	| "fr-CA"
+	| "fr-FR"
+	| "he-IL"
+	| "hr-HR"
+	| "hu-HU"
+	| "is-IS"
+	| "it-IT"
+	| "ja-JP"
+	| "km-KH"
+	| "ko-KR"
+	| "lt-LT"
+	| "lv-LV"
+	| "mn-MN"
+	| "nb-NO"
+	| "nl-NL"
+	| "pl-PL"
+	| "pt-BR"
+	| "pt-PT"
+	| "ro-RO"
+	| "ru-RU"
+	| "sk-SK"
+	| "sl-SI"
+	| "sr-RS"
+	| "sv-SE"
+	| "th-TH"
+	| "tr-TR"
+	| "uk-UA"
+	| "vi-VN"
+	| "zh-CN"
+	| "zh-TW";
 
 export interface Locale {
-  title?: string;
-  description?: string;
-  locale: Lang; // https://github.com/anton-bot/locale-enum
-  punctuationInQuote: boolean;
-  terms: Term[];
+	title?: string;
+	description?: string;
+	locale: Lang; // https://github.com/anton-bot/locale-enum
+	punctuationInQuote: boolean;
+	terms: Term[];
 }
 
 export interface StandaloneLocale extends Locale {
-  translators?: string[];
-  description?: string;
-  rights?: string; // use enum here, and in style
+	translators?: string[];
+	description?: string;
+	rights?: string; // use enum here, and in style
 }
 
 export interface Term {
-  name: string;
-  form?: "short" | "symbol";
-  single?: string;
-  multiple?: string; // TODO this and above need to be coupled
+	name: string;
+	form?: "short" | "symbol";
+	single?: string;
+	multiple?: string; // TODO this and above need to be coupled
 }
 
 export interface DateTerm extends Term {
-  dateParts: string[]; // TODO
+	dateParts: string[]; // TODO
 }

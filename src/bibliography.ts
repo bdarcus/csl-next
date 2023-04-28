acef4d376edb4fcfc6b05eb5bb45422bdb7e6da5
Bruce D'Arcus <bdarcus@gmail.com>
2023-04-28 17:25:48 -0400

    feat(style): add DateFormatting config
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- src/bibliography.ts
+++ src/bibliography.ts
@@ -1,8 +1,47 @@
+import { Type } from "class-transformer";
 import { Reference } from "./reference";
 
+/**
+ * A bibliography is a collection of references.
+ *
+ * It is the input of a citation processor.
+ *
+ * @examples
+ * {
+ * "title": "My Bibliography",
+ * "description": "A collection of references.",
+ * "references": [
+ *   {
+ *     "id": "doe1",
+ *     "type": "book",
+ *    "title": "The Title",
+ *     "author": [
+ *       {
+ *         "family": "Doe",
+ *         "given": "Jane"
+ *       }
+ *     ],
+ *     "issued": "2023"
+ *   }
+ * ]
+ *}
+ */
 export class Bibliography {
+	/**
+	 * The title of the bibliography.
+	 */
 	title?: string;
+	/**
+	 * The description of the bibliography.
+	 */
 	description?: string;
+
+	/**
+	 * The references array.
+	 *
+	 * @items.minimum 1
+	 */
+	//@Type(() => Reference) // FIX getting an area; seems a bug elsewhere
 	references: Reference[];
 
 	constructor(title?: string, description?: string) {
@@ -13,9 +52,5 @@ export class Bibliography {
 
 	addReference(reference: Reference): void {
 		this.references.push(reference);
-	}
-
-	addReferences(references: Reference[]): void {
-		this.references = this.references.concat(references);
 	}
 }

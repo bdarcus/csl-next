e9775a77bcfd06e880de1e564d1f4bd3b64febd1
Bruce D'Arcus <bdarcus@gmail.com>
2023-04-28 22:42:10 -0400

    fix(processor): loadJSON and loadYAML work
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { Bibliography } from "./bibliography";
 import { loadJSON, loadYAML } from "./utils";
 
 const bibj = loadJSON("examples/bibliography.json");
-//const biby = loadYAML("examples/bibliography.yaml"); // why error?
+const biby = loadYAML("examples/bibliography.yaml"); // why error?
 
 console.log("The JSON bibliography is:\n", bibj);
 

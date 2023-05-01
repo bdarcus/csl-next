fc8b93c9b6ba8c8128e40cf082d26672c70cf02b
Bruce D'Arcus <bdarcus@gmail.com>
2023-05-01 04:16:26 -0400

    chore(tests): setup tests
    Add node test-runner-based setup, with placeholder example.
    
    It allows testing the typescript directly, and avoids more complicated
    external testing packages.
    
    Uses the new v20 of node.
    
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

---
+++ tests/processor.test.ts
@@ -0,0 +1,27 @@
+import { equal } from "node:assert/strict";
+import { describe, it } from "node:test";
+import { Processor } from "../src/processor.ts";
+
+describe("sorting", () => {
+	it.todo("sorts references by author");
+
+	it.todo("sorts references by author, year");
+
+	it.todo("sorts references as cited");
+});
+
+describe("grouping", () => {
+	it.todo("groups references by author");
+
+	it.todo("groups references by author, year");
+
+	it.todo("sorts references as cited");
+});
+
+describe("contributor formatting", () => {
+	it.todo("formats authors");
+});
+
+describe("dates formatting", () => {
+	it.todo("formats issued dates");
+});

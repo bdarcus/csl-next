c6dcfcddd8d6d895dec481af8fd2085498395010
Bruce D'Arcus <bdarcus@gmail.com>
2023-05-02 20:05:48 -0400

    style: formatting
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- build/esbuild.cjs
+++ build/esbuild.cjs
@@ -4,51 +4,51 @@ const esbuild = require("esbuild");
 
 /** @type esbuild.BuildOptions */
 const devConfig = {
-  sourcemap: "linked",
+	sourcemap: "linked",
 };
 
 /** @type esbuild.BuildOptions */
 const prodConfig = {
-  minify: true,
+	minify: true,
 };
 
 /** @type esbuild.BuildOptions */
 const config = {
-  entryPoints: ["src/main.ts"],
-  outfile: "dist/main.cjs",
-  bundle: true,
-  platform: "node",
-  logLevel: "info",
+	entryPoints: ["src/main.ts"],
+	outfile: "dist/main.cjs",
+	bundle: true,
+	platform: "node",
+	logLevel: "info",
 
-  define: {
-    VERSION: JSON.stringify(process.env.npm_package_version),
-    DEVELOP: JSON.stringify(!!argv.dev),
-  },
+	define: {
+		VERSION: JSON.stringify(process.env.npm_package_version),
+		DEVELOP: JSON.stringify(!!argv.dev),
+	},
 
-  plugins: [],
+	plugins: [],
 
-  metafile: argv.meta,
-  ...(argv.dev ? devConfig : prodConfig),
+	metafile: argv.meta,
+	...(argv.dev ? devConfig : prodConfig),
 };
 
 if (argv.run)
-  config.plugins.push(
-    require("@es-exec/esbuild-plugin-start").default({
-      script: "node dist/main.cjs",
-    }),
-  );
+	config.plugins.push(
+		require("@es-exec/esbuild-plugin-start").default({
+			script: "node dist/main.cjs",
+		}),
+	);
 
 if (argv.watch) {
-  (async () => {
-    const ctx = await esbuild.context(config);
-    await ctx.watch();
-  })();
+	(async () => {
+		const ctx = await esbuild.context(config);
+		await ctx.watch();
+	})();
 } else {
-  esbuild.build(config).then((file) => {
-    if (argv.meta)
-      require("fs").writeFileSync(
-        "dist/meta.json",
-        JSON.stringify(file.metafile),
-      );
-  });
+	esbuild.build(config).then((file) => {
+		if (argv.meta)
+			require("fs").writeFileSync(
+				"dist/meta.json",
+				JSON.stringify(file.metafile),
+			);
+	});
 }

1f0a7bf04f30488a7884114f1f53186358c4122a
Bruce D'Arcus <bdarcus@gmail.com>
2023-05-11 06:39:11 -0400

    refactor!(locale): make key-object
    Also, add a number of types to provide the keys.
    
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- src/locale.ts
+++ src/locale.ts
@@ -1,4 +1,4 @@
-// REVIEW not sure where best to do this
+// REVIEW replace with https://esm.sh/locale-enum@1.1.1
 type Lang =
   | "af-ZA"
   | "ar"
@@ -48,27 +48,121 @@ type Lang =
   | "zh-CN"
   | "zh-TW";
 
-export interface Locale {
+/**
+ * The unique human-readable identifier for a term.
+ */
+export type LocalizedTermName =
+  | LocalizedTermNameLocator
+  | LocalizedTermNameLocatorNumber
+  | LocalizedTermNameMisc;
+
+export type LocalizedTermNameMisc =
+  | "accessed"
+  | "ad"
+  | "advance-online-publication"
+  | "album"
+  | "and"
+  | "and-others"
+  | "anonymous"
+  | "at"
+  | "audio-recording"
+  | "available-at"
+  | "bc"
+  | "bce"
+  | "by"
+  | "ce"
+  | "circa"
+  | "cited"
+  | "et-al"
+  | "film"
+  | "forthcoming"
+  | "from"
+  | "henceforth"
+  | "ibid"
+  | "in"
+  | "in-press"
+  | "internet"
+  | "interview"
+  | "letter"
+  | "loc-cit"
+  | "no date"
+  | "no-place"
+  | "no-publisher"
+  | "on"
+  | "online"
+  | "op-cit"
+  | "original-work-published"
+  | "personal-communication"
+  | "podcast"
+  | "podcast-episode"
+  | "preprint"
+  | "presented-at"
+  | "radio-broadcast"
+  | "radio-series"
+  | "radio-series-episode"
+  | "reference"
+  | "retrieved"
+  | "review-of"
+  | "scale"
+  | "special-issue"
+  | "special-section"
+  | "television-broadcast"
+  | "television-series"
+  | "television-series-episode"
+  | "video"
+  | "working-paper";
+
+export type LocalizedTermNameLocator =
+  | "act"
+  | "appendix"
+  | "article-locator"
+  | "book"
+  | "canon"
+  | "chapter"
+  | "column"
+  | "elocation"
+  | "equation"
+  | "figure"
+  | "folio"
+  | "line"
+  | "note"
+  | "opus"
+  | "paragraph"
+  | "rule"
+  | "scene"
+  | "sub-verbo"
+  | "table"
+  | "timestamp"
+  | "title-locator"
+  | "verse";
+
+export type LocalizedTermNameLocatorNumber =
+  | "issue"
+  | "page"
+  | "part"
+  | "section"
+  | "supplement"
+  | "version"
+  | "volume";
+
+export type LocalizedTermFormat = "short" | "symbol";
+
+export interface Localization {
   title?: string;
   description?: string;
-  locale: Lang; // https://github.com/anton-bot/locale-enum
-  punctuationInQuote: boolean;
-  terms: Term[];
+  locale: Lang;
+  punctuationInQuote?: boolean;
+  terms: Record<LocalizedTermName, LocalizedTerm>;
 }
 
-export interface StandaloneLocale extends Locale {
+export interface StandaloneLocalize extends Localization {
   translators?: string[];
   description?: string;
   rights?: string; // use enum here, and in style
 }
 
-export interface Term {
-  name: string;
-  form?: "short" | "symbol";
+export interface LocalizedTerm {
+  format?: LocalizedTermFormat;
   single?: string;
   multiple?: string; // TODO this and above need to be coupled
-}
-
-export interface DateTerm extends Term {
-  dateParts: string[]; // TODO
 }

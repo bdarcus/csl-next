618e679cd66221e9375a9af7c22a2141e578a978
Bruce D'Arcus <bdarcus@gmail.com>
2023-05-12 14:59:14 -0400

    fix(schemas): run off npm again
    Fix: #111
    
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- scripts/make_schemas.ts
+++ scripts/make_schemas.ts
@@ -3,23 +3,23 @@ import { exec } from "https://deno.land/x/exec/mod.ts";
 
 const configs = [
   {
-    path: "./src/style.ts",
+    path: "./npm/src/style.ts",
     type: "Style",
     tsconfig: "tsconfig.json",
   },
 
   {
-    path: "./src/reference.ts",
+    path: "./npm/src/reference.ts",
     type: "InputReference",
     tsconfig: "tsconfig.json",
   },
   {
-    path: "./src/bibliography.ts",
+    path: "./npm/src/bibliography.ts",
     type: "InputBibliography",
     tsconfig: "tsconfig.json",
   },
   {
-    path: "./src/citation.ts",
+    path: "./npm/src/citation.ts",
     type: "Citation",
     tsconfig: "tsconfig.json",
   },
@@ -31,7 +31,7 @@ for (const config of configs) {
     config["type"].toLowerCase()
   }-schema.json`;
   const command =
-    `ts-json-schema-generator --no-type-check --path ${config.path} --type ${config.type} --tsconfig ${config.tsconfig} --out ${schemaFileName}`;
+    `ts-json-schema-generator --no-type-check --path ${config.path} --type ${config.type} --out ${schemaFileName}`;
 
   console.log(`Generating ${schemaFileName}...`);
   console.log(command);

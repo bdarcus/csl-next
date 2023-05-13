4b1613ce4ef3d02a20b4a281aad8b4273afe4701
Bruce D'Arcus <bdarcus@gmail.com>
2023-05-13 13:36:26 -0400

    refactor(utils): split loadYAML in two
    And switch to Deno native function.
    
    I tried to make these functions async, but couldn't figure out how to
    resolve the Promise so the tests ran correctly.
    
    Should revisit later.
    
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,23 @@
-import * as fs from "node:fs";
 import { yaml } from "../deps.ts";
 import { InputBibliography } from "./bibliography.ts";
+import { Style } from "./style.ts";
 
-export function loadJSON(path: string): InputBibliography {
-  const jdata = fs.readFileSync(path, "utf8");
-  return JSON.parse(jdata);
+export function loadBibliography(
+  // REVIEW these functions should probably be async, but
+  // I can't get it working correctly
+  path: string,
+): InputBibliography {
+  const bibliography = yaml.parse(
+    Deno.readTextFileSync(path),
+  ) as InputBibliography;
+  return bibliography;
 }
 
-export function loadYAML(path: string): Record<string, unknown> {
-  const ydata = fs.readFileSync(path, "utf8");
-  return yaml.parse(ydata);
+export function loadStyle(
+  path: string,
+): Style {
+  const style = yaml.parse(
+    Deno.readTextFileSync(path),
+  ) as Style;
+  return style;
 }

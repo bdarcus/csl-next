5d1f8c1b6161523ed5191cd6e2d74d1424a4bdaf
Bruce D'Arcus <bdarcus@gmail.com>
2023-05-30 15:42:14 -0400

    refactor(citation): locator is only for vars
    Signed-off-by: Bruce D'Arcus <bdarcus@gmail.com>

--- src/locator.ts
+++ src/locator.ts
@@ -1,14 +1,4 @@
-// CSL locators model
-// A locator is a simple key-value object, where the value is some potentially discontinuos list of numbers and/or strings.
-// Examples:
-//   - page 1
-//   - page 23, 25-36
-//   - chapter IV
-
-/** A key-value object, or a string. */
-export type Locator = Record<LocatorTerms, string> | string;
-
-type LocatorTerms =
+export type LocatorTerms =
   | "book"
   | "chapter"
   | "column"
